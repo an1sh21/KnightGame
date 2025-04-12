740231cdd4ba6a37f697746dfe6847378350b009
an1sh21 <107747127+an1sh21@users.noreply.github.com>
2025-04-12 21:31:19 +0530

    Updating Dying 2.0

--- script/killzone.gd
+++ script/killzone.gd
@@ -7,8 +7,11 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You Died")
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start() # Replace with function body.
 
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene() # Replace with function body.
