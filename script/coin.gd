aace944e6b3ab1a18a9e0484cfc8b5f6698ac1f1
an1sh21 <107747127+an1sh21@users.noreply.github.com>
2025-04-15 22:25:05 +0530

    Pick Up sound

--- script/coin.gd
+++ script/coin.gd
@@ -3,9 +3,13 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 @onready var game_manager: Node = %"Game Manager"
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 
 func _on_body_entered(body: Node2D) -> void:
 	game_manager.add_point()
-	queue_free() # Replace with function body.
+	animation_player.play("Pickup")
+	
+	
+	
